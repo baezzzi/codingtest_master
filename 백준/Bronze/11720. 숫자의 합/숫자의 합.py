e819a32aec698942c119bcd14466bc00e119a07f
100 <130140330+baezzzi@users.noreply.github.com>
2025-04-09 17:45:48 +0900

    [Bronze IV] Title: 숫자의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -0,0 +1,13 @@
+def __init__() :
+    num = int(input()) # 숫자 개수 입력
+
+    number = input()
+
+    result = 0
+
+    for i in range(num) :
+        result += int(number[i])
+    
+    print(result)
+
+__init__()
