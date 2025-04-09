94257eb8dc0445be62ae00a5744287a49189d7b6
100 <130140330+baezzzi@users.noreply.github.com>
2025-04-09 16:57:23 +0900

    [Bronze V] Title: 문자열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -0,0 +1,28 @@
+result = []
+
+def firstNend(word) : 
+    wordlen = len(word)
+    
+    if(wordlen > 1000) :
+        return 0
+    
+    feword = word[0]+word[wordlen-1]
+
+    result.append(feword)
+
+def __init__() :
+    num = int(input()) # 테스트케이스 개수 입력
+    if (num < 1 or num > 10) :
+        return 0
+
+    for i in range(num) :
+        word = input()
+        if (len(word) > 1000) :
+            return 0
+        firstNend(word)
+    
+    for i in range(num) :
+        print(result[i])
+
+__init__()
+    
