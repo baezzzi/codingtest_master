050af8e7a3a6a23a3af21813ecc6df05a12bb9a7
100 <mycodingnumber100@gmail.com>
2025-04-14 16:17:26 +0900

    [Bronze II] Title: 단어의 개수, Time: 48 ms, Memory: 39824 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -0,0 +1,11 @@
+## 1152번 단어의 개수
+def countWords(words) :
+    word = words.split()
+    count = len(word)
+    print(count)
+
+def __init__() :
+    words = input()
+    countWords(words)
+
+__init__()
