0812be2a7fa4ab1d0c68aa58c1243d2962ca77cd
JI YEON BAEK (100) <mycodingnumber100@gmail.com>
2025-05-19 16:10:08 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,14 @@
+def main() :
+    N = int(input()) 
+
+    numbers = []
+
+    for _ in range(N) :
+        numbers.append(int(input()))
+
+    numbers.sort()
+
+    for n in numbers :
+        print(n)
+
+main()
